15eabfa6f21c0dedc9fae928a94e88056c9348d2
Dieudonne00 <146090340+Dieudonne00@users.noreply.github.com>
2026-01-18 17:09:20 +0200

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.kigalicarhire.rw";
+  const baseUrl = "https://www.kigalicarrental.site";
 
   return [
     {
