aabef6e230bce4a7610eefbb127bb201b9c052a0
Dieudonne00 <146090340+Dieudonne00@users.noreply.github.com>
2026-01-18 15:13:56 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import HeroSection from "@/components/HeroSection";
-
+import AboutSection from "@/components/AboutSection";
 import FeaturedFleet from "@/components/FeaturedFleet";
 import ServicesSection from "@/components/ServicesSection";
 import FeaturedBlogs from "@/components/FeaturedBlogs";
