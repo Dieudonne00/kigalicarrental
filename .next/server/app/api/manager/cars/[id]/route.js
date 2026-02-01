c00fe8737d48fa7b11f8acd4cfdf6cdb69756bec
Dieudonne00 <146090340+Dieudonne00@users.noreply.github.com>
2026-02-01 06:13:00 +0000

    Fix merge conflicts, clean API routes, ready for Vercel deployment

--- .next/server/app/api/manager/cars/[id]/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../../chunks/[turbopack]_runtime.js")("server/app/api/manager/cars/[id]/route.js")
-R.c("server/chunks/[root-of-the-server]__7d8ff140._.js")
-R.c("server/chunks/[root-of-the-server]__f408c708._.js")
-R.c("server/chunks/_next-internal_server_app_api_manager_cars_[id]_route_actions_7771000c.js")
-R.m(83723)
-module.exports=R.m(83723).exports
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"G/LZrtMxSDqF/XfGlTCqW2+i3RbqeE784EqR9ws1IRc=\"\n}"
